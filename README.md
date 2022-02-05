9887289c9d0ca83b31d3cd57005d6cb40fcec859
Joao Luis Meloni Assirati <jlmassir@gmail.com>
2022-02-05 00:03:14 -0300

    Improve documentation

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ C library for large floating point sums
 Features:
 * Online, non-recursive pairwise summation (cascade summation)
 * Kahan summation
+
+See psum_test.c and ksum_test.c for pairwise and Kahan summation examples.
--- lsum.c
+++ lsum.c
@@ -3,6 +3,7 @@
   Features:
   * Online, non-recursive pairwise summation (cascade summation)
   * Kahan summation
+  See psum_test.c and ksum_test.c for pairwise and Kahan summation examples.
 
   Copyright (C) 2022  Joao Luis Meloni Assirati.
 
